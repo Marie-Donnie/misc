da6dd608654b54050686e05e9d834e5784176f1c
Marie-Donnie <aldanna.riel@gmail.com>
2016-06-15 10:29:02 +0200

    Comments

--- scripts/nova/resultsjson.py
+++ scripts/nova/resultsjson.py
@@ -1,9 +1,14 @@
+# Script to get the logs from lines of json to a global json format
+
 import json
 
+
+
 with open("/opt/logs/db_api.log", "r") as logs:
+    # put each lines into a list
     lines = logs.readlines()
+    # remove the nasty new lines
     lines = map(lambda s: s.replace('\n',''), lines)
-    #print(lines)
     dicts = map(json.loads, lines)
     with open("/opt/logs/db_api.json","w") as fjson:
         fjson.write(json.dumps(dicts))
