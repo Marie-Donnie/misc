bd550a7c0de70066a44a956d659dd91d79ea4a94
Marie-Donnie <aldanna.riel@gmail.com>
2016-06-15 16:05:55 +0200

    Finalizing the results

--- scripts/nova/compute.py
+++ scripts/nova/compute.py
@@ -2,29 +2,35 @@
 
 import json
 import pandas as pd
-import matplotlib.pyplot as plt
+import matplotlib.pyplot as pl
+import os 
+
 
 with open("/opt/logs/db_api_disco.json", "r") as discojson:
     with open("/opt/logs/db_api_mysql.json", "r") as mysqljson:
+        # deserialize files
         ddicts = json.load(discojson)
         mdicts = json.load(mysqljson)
+        # create pandas dataframes
         ddf = pd.DataFrame(ddicts)
         mdf = pd.DataFrame(mdicts)
-        # print(df)                  
-        # print(df.describe())
+        # group by method and calculate the average duration
         dduree = ddf.groupby("method").mean()
         mduree = mdf.groupby("method").mean()
+        # keep only duration, remove timestamp
         dduree2 = dduree.loc[:,["duration"]]
         mduree2 = mduree.loc[:,["duration"]]
-        # print(duree2)
-        # duree2.plot.bar()
-        # plt.show()
-        # pduree = pd.merge(dduree2, mduree2, how='right')
-        # print(mduree)
-        # print(dduree)
+        # rename the columns
         dduree2.columns = ['disco']
         mduree2.columns = ['mysql']
+        # join the dataframes
         pduree = mduree2.join(dduree2)
-        print(pduree)
-        pduree.plot.bar(stacked=True)
-        plt.show()
+        pduree["difference"] = pduree["disco"] - pduree["mysql"]
+        # save the results
+        chemin = os.path.dirname(os.path.realpath(__file__))
+        with open(chemin+"/results.txt","w") as results:
+            results.write(pduree.to_string())
+        # testing functions
+        # print(pduree)
+        # pduree.plot.bar(stacked=True)
+        # plt.show()
