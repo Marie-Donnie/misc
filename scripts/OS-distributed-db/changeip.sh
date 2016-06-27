a97646700a8868a78103a995aee7c9d32bed1a2a
Marie-Donnie <aldanna.riel@gmail.com>
2016-06-27 17:55:55 +0200

    Added some comments

--- scripts/OS-distributed-db/changeip.sh
+++ scripts/OS-distributed-db/changeip.sh
@@ -1,13 +1,14 @@
 #!/bin/bash
 
-
+# gets the ip
 ipfile="$1"
 grep "inet addr:" $ipfile | cut -d: -f2 | awk '{ print $1}' > ips.txt
 ipmysql=$(head -1 ips.txt)
 ipredis=$(tail -1 ips.txt)
 echo $ipmysql
 echo $ipredis
-           
+
+# stores them in the discovery-vagrant scripts for deployment
 cmd="sed -i 's\/\^redis_cluster_enabled: False\$\/\&\\\nnodes=${ipredis}\/' rome.conf"
 
 sed -i "s/^EOM$/MYSQL_HOST=${ipmysql}\n&/" discovery-vagrant/05_devstack.sh
