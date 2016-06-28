90347e180a985c99d382320ffaf462f4f1fa801b
Marie-Donnie <aldanna.riel@gmail.com>
2016-06-28 10:06:48 +0200

    More precise comments

--- scripts/OS-distributed-db/changeip.sh
+++ scripts/OS-distributed-db/changeip.sh
@@ -1,6 +1,7 @@
 #!/bin/bash
 
-# gets the ip
+# gets the ips from the file given in argument, which is supposed to have received
+# the result for "ifconfig eth0" from both VMs.
 ipfile="$1"
 grep "inet addr:" $ipfile | cut -d: -f2 | awk '{ print $1}' > ips.txt
 ipmysql=$(head -1 ips.txt)
@@ -10,6 +11,9 @@ echo $ipredis
 
 # stores them in the discovery-vagrant scripts for deployment
 cmd="sed -i 's\/\^redis_cluster_enabled: False\$\/\&\\\nnodes=${ipredis}\/' rome.conf"
-
+  # add a "MYSQL_HOST=<IpAddressForMySqlDB>" before EOM in file 05_devstack.sh
 sed -i "s/^EOM$/MYSQL_HOST=${ipmysql}\n&/" discovery-vagrant/05_devstack.sh
+  # add a "sed -i s/^redis_cluster_enabled: False$/&\nnodes=<IpAdressForRedisDB>/ rome.conf"
+  # line to 02_rome.sh which will enable to put the ip adress in rome.conf during the
+  # discovery-vagrant deployment.
 sed -i "s/^pushd rome\s*$/&\n${cmd}/" discovery-vagrant/02_rome.sh
