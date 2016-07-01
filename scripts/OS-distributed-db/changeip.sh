5fd6916f72c6773fca364df453452fe168393a37
Marie-Donnie <aldanna.riel@gmail.com>
2016-07-01 16:20:40 +0200

    Changes to adapt the new version of the deployment
    !DOES NOT WORK!

--- scripts/OS-distributed-db/changeip.sh
+++ scripts/OS-distributed-db/changeip.sh
@@ -6,16 +6,14 @@
 
 # gets the ips from the file
 ipfile="$1"
-grep "inet addr:" $ipfile | cut -d: -f2 | awk '{ print $1}' > ips.txt
-ipmysql=$(head -1 ips.txt)
-ipredis=$(tail -1 ips.txt)
-echo $ipmysql
-echo $ipredis
+grep "inet addr:" $ipfile | cut -d: -f2 | awk '{ print $1}' > ip.txt
+ip=$(head -1 ip.txt)
+echo $ip
 
 # stores them in the discovery-vagrant scripts for deployment
-cmd="sed -i 's\/\^redis_cluster_enabled: False\$\/\&\\\nnodes=${ipredis}\/' rome.conf"
+cmd="sed -i 's\/\^redis_cluster_enabled: False\$\/\&\\\nnodes=${ip}\/' rome.conf"
   # add a "MYSQL_HOST=<IpAddressForMySqlDB>" before EOM in file 05_devstack.sh
-sed -i "s/^EOM$/MYSQL_HOST=${ipmysql}\nMYSQL_USER=stack\nMYSQL_PASSWORD=devstack\n&/" discovery-vagrant/05_devstack.sh
+sed -i "s/^EOM$/MYSQL_HOST=${ip}\nMYSQL_USER=stack\nMYSQL_PASSWORD=devstack\n&/" discovery-vagrant/05_devstack.sh
   # add a "sed -i s/^redis_cluster_enabled: False$/&\nnodes=<IpAdressForRedisDB>/ rome.conf"
   # line to 02_rome.sh which will enable to put the ip adress in rome.conf during the
   # discovery-vagrant deployment.
--- scripts/OS-distributed-db/os-distri-db.py
+++ scripts/OS-distributed-db/os-distri-db.py
@@ -7,64 +7,81 @@ from execo_g5k import oar
 from execo_engine import logger
 from execo.time_utils import *
 from execo.process import *
+from optparse import OptionParser
 
 plan = ex5.planning
-grid = ex5.oargrid
 ky = ex5.kadeploy
 
-okay = ["paravance","grisou","graphene","griffon"]
-exclu = ex5.api_utils.get_g5k_clusters()
-# list that contains all clusters excepts those in okay list
-excluded = [c for c in exclu if c not in okay]
 
-end = ex.time_utils.format_date(time.time()+6000)
+end = ex.time_utils.format_date(time.time()+11000)
 
 # must have a .env on the frontend that will deploy the ubuntu 
 envfile = "envdb/monubuntu.env"
 
-try:
-    # makes a reservation
-    logger.info("Making a reservation")
-    planning = plan.get_planning(endtime=end)
-    slots = plan.compute_slots(planning, walltime="01:30:00", excluded_elements=excluded)
-    startdate, enddate, resources = plan.find_free_slot(slots, {'grid5000':3})                    
-    resources = plan.distribute_hosts(resources, {'grid5000':3}, excluded_elements=excluded)
-    if startdate is None:
-        raise ReservationError("Could not find a slot for the requested resources.")
-    specs = plan.get_jobs_specs(resources, excluded_elements=excluded)
-    # print("Using sites : %s" % resources)
-    subs, _ = grid.oargridsub(specs, walltime="01:30:00", job_type='deploy')
-    if subs is None:
-        raise ReservationError("No oargrid job was created.")
-    else:
-        logger.info("Using new oargrid job : %s" % subs)
-    jobs = grid.get_oargrid_job_oar_jobs(subs)
-    nodes = grid.get_oargrid_job_nodes(subs)
-    for job in jobs:
-        logger.info("Job id : %s, site : %s" % (job[0], job[1]))
-    logger.info("Using nodes : %s" % nodes)
+class os-distri-db():
 
-    # deploys
-    logger.info("Deploying ubuntu on nodes")
-    deployment = ky.Deployment(hosts=nodes, env_file=envfile)
-    deployed_hosts, _ = ky.deploy(deployment)
-    logger.info("Deployed on %s" % deployed_hosts)
-    if len(deployed_hosts) == 0:
-        raise DeploymentError("Error while deploying")
-    
-    # prepares disco-vagrant
-    main = nodes[0]
-    db = [nodes[1], nodes[2]]
-    logger.info("Discovery-vagrant will deploy on : %s" % main)
-    logger.info("The databases will be stored on : %s" % db)
+    def __init__(self):
+        """Define options for the experiment"""
+        parser = OptionParser()
+        parser.add_option("-f", "--full", dest="verbose", default=False,
+                  help="Run both implementations (default: %(defaults))")
+        parser.add_option("-i", "--implementation", dest="verbose", default=False,
+                          help="Run the deployment for the specified implementation (default: %(defaults))")
+        (self.options, self.args) = parser.parse_args()
+
+    def run(self):
+        """Perform experiment"""
+        print("Options : %s" % self.options)
+
+            
+        try:
+            # makes a reservation                                 
+            logger.info("Making a reservation")
+            job = ex5.oarsub([( ex5.OarSubmission(resources = "/cluster=1/nodes=2", walltime="03:00:00", job_type='deploy'), "rennes")])
+            job_id, site = job[0]
+            if job_id is None:
+                raise ValueError("Could not find a slot for the requested resources.")
+            logger.info("Using new oar job : %s, on site : %s" % (job_id, site))
+            logger.info("Waiting for job to start")
+            nodes = ex5.get_oar_job_nodes(job_id, site)
+            logger.info("Using nodes : %s" % nodes)
+            logger.info("Reservation done")
+            
+        except Exception as e:
+            t, value, tb = sys.exc_info()
+            print str(t) + " " + str(value)
+            traceback.print_tb(tb)
+
+        try:
+            for impl in self.args[1:]:
+                if impl not in {"mysql", "disco"}:
+                    logger.info("Use only mysql or disco arguments")
+                    self.parser.print_help()
+                self.deploys()
+                self._disco_vag(impl)
+            
+    def deploys(self):
+        # deploys
+        logger.info("Deploying ubuntu on nodes")
+        deployment = ky.Deployment(hosts=nodes, env_file=envfile)
+        deployed_hosts, _ = ky.deploy(deployment)
+        logger.info("Deployed on %s" % deployed_hosts)
+        if len(deployed_hosts) == 0:
+            raise DeploymentError("Error while deploying")
+
+    def _disco_vag(self, impl="disco"):
+        # prepares disco-vagrant
+        main = nodes[0]
+        db = nodes[1]
+        logger.info("Discovery-vagrant will deploy on : %s, for %s implementation" % (main, impl)
+        logger.info("The databases will be stored on : %s" % db)
     
     # gets the ips for the database and stores them into a file
-    for node in db:
-        ifconfig = ex.process.SshProcess("ifconfig eth0", node, connection_params={'user':'ci'})
-        ifconfig.run()
-        with open("ip.txt", "a") as ipfile:
-            ipfile.write(ifconfig.stdout)
-    logger.info("Ips stored")
+    ifconfig = ex.process.SshProcess("ifconfig eth0", db, connection_params={'user':'ci'})
+    ifconfig.run()
+    with open("ip.txt", "a") as ipfile:
+        ipfile.write(ifconfig.stdout)
+    logger.info("Ip stored")
     
     # copies the file to the main vm
     ex.action.Put([main], ["ip.txt"], connection_params={'user':'ci'}).run()
@@ -91,6 +108,13 @@ try:
     ex.action.Remote("cd rally-vagrant ; python rally-g5k.py config.json /home/ci/jenkins/workspace/Rally-G5k/rally/samples/tasks/scenarios/nova/create-and-delete-floating-ips-bulk.json", main, connection_params={'user':'ci'}).run()
     logger.info("Tests finished")
 
+    # relaunches the test for sqlalchemy
+    ex.action.Remote("sed -i 's/IMPL = ApiProxy(False)/IMPL = ApiProxy(True)/' nova/db/api.py", main, connection_params={'user':'ci'}).run()    
+
+    path = "/opt/logs/db_api_" + impl
+    ex.action.Get(db, /opt/logs/
+
+    
     # gets the results
     # logger.info("Analyzing the results")    
     # ex.action.Remote("git clone https://github.com/Marie-Donnie/misc.git", main, connection_params={'user':'ci'}).run()
