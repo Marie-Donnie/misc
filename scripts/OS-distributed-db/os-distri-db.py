78a470230e9a8175888cd637ed39cfa610c21c7c
Marie-Donnie <aldanna.riel@gmail.com>
2016-07-04 11:50:02 +0200

    Not yet tested

--- scripts/OS-distributed-db/os-distri-db.py
+++ scripts/OS-distributed-db/os-distri-db.py
@@ -1,3 +1,20 @@
+"""Argus.
+
+Usage:
+    os-distri-db.py reservation
+    os-distri-db.py run (--full | -i <impl>)
+    os-distri-db.py (-h | --help)
+    os-distri-db.py --version
+
+Options:
+    -h --help     Show this screen
+    --version     Show version
+    --full        Run tests for both implementation
+    -i <impl>     Choose implementation 
+
+
+"""
+
 import os, sys
 import time
 import traceback
@@ -7,7 +24,7 @@ from execo_g5k import oar
 from execo_engine import logger
 from execo.time_utils import *
 from execo.process import *
-from optparse import OptionParser
+from docopt import docopt
 
 plan = ex5.planning
 ky = ex5.kadeploy
@@ -22,12 +39,20 @@ class os-distri-db():
 
     def __init__(self):
         """Define options for the experiment"""
-        parser = OptionParser()
-        parser.add_option("-f", "--full", dest="verbose", default=False,
-                  help="Run both implementations (default: %(defaults))")
-        parser.add_option("-i", "--implementation", dest="verbose", default=False,
-                          help="Run the deployment for the specified implementation (default: %(defaults))")
-        (self.options, self.args) = parser.parse_args()
+        argus = docopt(__doc__, version = 'Deployments 1.0')
+        print(argus)
+
+        self.resa_only = False
+        self.implementation = []
+        if argus["--full"]:
+            self.implementation = ["disco", "mysql"]
+        else:
+            if argus["-i"]:
+                self.implementation = [argus["-i"]]
+            else:
+                self.resa_only = True
+        print(self.resa_only)
+        print(self.implementation)
 
     def run(self):
         """Perform experiment"""
@@ -53,12 +78,18 @@ class os-distri-db():
             traceback.print_tb(tb)
 
         try:
-            for impl in self.args[1:]:
-                if impl not in {"mysql", "disco"}:
-                    logger.info("Use only mysql or disco arguments")
-                    self.parser.print_help()
-                self.deploys()
-                self._disco_vag(impl)
+            if not self.resa_only:
+                for impl in self.implementation:
+                    print(impl)
+                    if impl not in {"mysql", "disco"}:
+                        raise ValueError("Use only mysql or disco arguments")
+                    self.deploys()
+                    self._disco_vag(impl)
+                    
+        except Exception as e:
+            t, value, tb = sys.exc_info()
+            print str(t) + " " + str(value)
+            traceback.print_tb(tb)
             
     def deploys(self):
         # deploys
@@ -75,44 +106,47 @@ class os-distri-db():
         db = nodes[1]
         logger.info("Discovery-vagrant will deploy on : %s, for %s implementation" % (main, impl)
         logger.info("The databases will be stored on : %s" % db)
-    
-    # gets the ips for the database and stores them into a file
-    ifconfig = ex.process.SshProcess("ifconfig eth0", db, connection_params={'user':'ci'})
-    ifconfig.run()
-    with open("ip.txt", "a") as ipfile:
-        ipfile.write(ifconfig.stdout)
-    logger.info("Ip stored")
-    
-    # copies the file to the main vm
-    ex.action.Put([main], ["ip.txt"], connection_params={'user':'ci'}).run()
-    logger.info("File copied")
-    ex.action.Remote("git clone https://github.com/Marie-Donnie/discovery-vagrant.git", main, connection_params={'user':'ci'}).run()
-    logger.info("Discovery-Vagrant cloned")
-    # handles the scripts, gives access to the databases
-    ex.action.Remote("wget https://raw.githubusercontent.com/Marie-Donnie/misc/master/scripts/OS-distributed-db/changeip.sh", main, connection_params={'user':'ci'}).run()
-    ex.action.Remote("wget https://raw.githubusercontent.com/Marie-Donnie/misc/master/scripts/OS-distributed-db/db-access.sh", db, connection_params={'user':'ci'}).run()    
-    logger.info("Scripts downloaded")
-    ex.action.Remote("chmod +x changeip.sh ; ./changeip.sh ip.txt", main, connection_params={'user':'ci'}).run()
-    ex.action.Remote("chmod +x db-access.sh ; sudo ./db-access.sh", db, connection_params={'user':'ci'}).run()
 
-    # uses discovery-vagrant with remote databases
-    logger.info("Deploying discovery devstack")
-    ex.action.Remote("cd discovery-vagrant ; ./deploy.sh", main, connection_params={'user':'ci'}).run()
-    logger.info("Disco OS deployed")
+        # gets the ips for the database and stores them into a file
+        ifconfig = ex.process.SshProcess("ifconfig eth0", db, connection_params={'user':'ci'})
+        ifconfig.run()
+        with open("ip.txt", "a") as ipfile:
+            ipfile.write(ifconfig.stdout)
+        logger.info("Ip stored")
+                    
+        # copies the file to the main vm
+        ex.action.Put([main], ["ip.txt"], connection_params={'user':'ci'}).run()
+        logger.info("File copied")
+        ex.action.Remote("git clone https://github.com/Marie-Donnie/discovery-vagrant.git", main, connection_params={'user':'ci'}).run()
+        logger.info("Discovery-Vagrant cloned")
+        # handles the scripts, gives access to the databases
+        ex.action.Remote("wget https://raw.githubusercontent.com/Marie-Donnie/misc/master/scripts/OS-distributed-db/changeip.sh", main, connection_params={'user':'ci'}).run()
+        ex.action.Remote("wget https://raw.githubusercontent.com/Marie-Donnie/misc/master/scripts/OS-distributed-db/db-access.sh", db, connection_params={'user':'ci'}).run()    
+        logger.info("Scripts downloaded")
+        ex.action.Remote("chmod +x changeip.sh ; ./changeip.sh ip.txt", main, connection_params={'user':'ci'}).run()
+        ex.action.Remote("chmod +x db-access.sh ; sudo ./db-access.sh", db, connection_params={'user':'ci'}).run()
 
-    # launches the tests
-    logger.info("Cloning rally-vagrant...")
-    ex.action.Remote("git clone https://github.com/BeyondTheClouds/rally-vagrant.git", main, connection_params={'user':'ci'}).run()
-    logger.info("Done")
-    logger.info("Executing tests")
-    ex.action.Remote("cd rally-vagrant ; python rally-g5k.py config.json /home/ci/jenkins/workspace/Rally-G5k/rally/samples/tasks/scenarios/nova/create-and-delete-floating-ips-bulk.json", main, connection_params={'user':'ci'}).run()
-    logger.info("Tests finished")
+        # make some changes for discovery-vagrant, since default uses ROME
+        if (impl="mysql"):
+            logger.info("Adjusting deployment for mysql")
+            ex.action.Remote("cd discovery-vagrant ; sed -i 's/NOVA_BRANCH=disco\/mitaka/NOVA_BRANCH=vanilla/' 05_devstack.sh", main, connection_params={'user':'ci'}).run()   
 
-    # relaunches the test for sqlalchemy
-    ex.action.Remote("sed -i 's/IMPL = ApiProxy(False)/IMPL = ApiProxy(True)/' nova/db/api.py", main, connection_params={'user':'ci'}).run()    
+        # uses discovery-vagrant with remote databases
+        logger.info("Deploying discovery devstack")
+        ex.action.Remote("cd discovery-vagrant ; ./deploy.sh", main, connection_params={'user':'ci'}).run()
+        logger.info("Disco OS deployed")
 
-    path = "/opt/logs/db_api_" + impl
-    ex.action.Get(db, /opt/logs/
+        # launches the tests
+        logger.info("Cloning rally-vagrant...")
+        ex.action.Remote("git clone https://github.com/BeyondTheClouds/rally-vagrant.git", main, connection_params={'user':'ci'}).run()
+        logger.info("Done")
+        logger.info("Executing tests")
+        ex.action.Remote("cd rally-vagrant ; python rally-g5k.py config.json /home/ci/jenkins/workspace/Rally-G5k/rally/samples/tasks/scenarios/nova/create-and-delete-floating-ips-bulk.json", main, connection_params={'user':'ci'}).run()
+        logger.info("Tests finished")
+
+   
+        # path = "/opt/logs/db_api_" + impl
+        # ex.action.Get(main, path, local_location="./results", connection_params={'user':'ci'})
 
     
     # gets the results
